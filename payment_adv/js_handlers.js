882f4e80393debd1b379e1be0c8597dcf646b540
Henuka Pathirana <henukapathirana2005@gmail.com>
2026-01-04 10:45:24 +0530

    Add confirmation dialog for payment deletion

--- payment_adv/js_handlers.js
+++ payment_adv/js_handlers.js
@@ -1,1 +1,10 @@
+document.addEventListener("click", function(e){
+    const delBtn = e.target.closest(".paymentDeleteBtn");
+    if(delBtn){
+        const id = delBtn.dataset.id;
+        if(confirm("Are you sure you want to delete this payment?")){
+            window.location = "delete.php?id=" + id;
+        }
+    }
+});
 
